8515ec496bd38cadea40a5f7a4360a8a7286e96d
chuanzhangjiang <chuanzhangjiang@outlook.com>
2020-10-05 23:27:35 +0800

    将背景图片设置转移至.xsession文件中

--- autostart.sh
+++ autostart.sh
@@ -2,9 +2,8 @@
 
 #状态栏显示设置
 /bin/bash ~/.dwm/dwm-status-refresh-task.sh &
+# feh -r --bg-fill ~/Pictures/Wallpapers/ #背景图片设置，暂时使用~/.xsession文件运行加载
 # 加载透明背景控件
 # compton -b
 xcompmgr &
-# 设置背景图片
-feh -r --bg-max Pictures/Wallpapers/
 
--- dwm-status-refresh-task.sh
+++ dwm-status-refresh-task.sh
@@ -2,7 +2,6 @@
 
 while true
 do
-    touch ~/autostart
     bash ~/.dwm/dwm-status-show-tool.sh
     sleep 2
 done
