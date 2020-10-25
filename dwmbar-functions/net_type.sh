e85fc623d8293daa7f7421056fceee48ccef6102
chuanzhangjiang <chuanzhangjiang@outlook.com>
2020-10-25 22:06:34 +0800

    bug fix

--- dwmbar-functions/net_type.sh
+++ dwmbar-functions/net_type.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 net_type_icon(){
-    NETTYPE="$(nmcli device status | grep -e '\sconnected\s' | sed 's/  */ /g' | cut -d ' ' -f 2)"
+    NETTYPE="$(nmcli device status | grep -e '\sconnected\s' | sed 's/  */ /g' | sed -n '1p' | cut -d ' ' -f 2)"
     if [ $NETTYPE = "wifi" ];then
 	printf "📶"
     elif [ $NETTYPE = "ethernet" ];then
 	printf "🌐"
     else
-	printf "❎"
+	printf " ❎"
     fi
 }
 
 net_name() {
-    printf "$(nmcli device status | grep -e '\sconnected\s' | sed 's/  */ /g' | cut -d ' ' -f 4)"
+    printf "$(nmcli device status | grep -e '\sconnected\s' | sed 's/  */ /g' | sed -n '1p' | cut -d ' ' -f 4)"
 }
 
 print_net_type(){
